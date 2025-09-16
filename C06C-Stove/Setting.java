ec6baa29e59b89beeb5f4898ec8e42075e6cd616
ethanlagrone <ethan_lagrone@mines.edu>
2025-09-16 14:43:11 -0600

    Created v1 of setting enum type

--- C06C-Stove/Setting.java
+++ C06C-Stove/Setting.java
@@ -1,4 +1,16 @@
 
-public class Setting {
+public enum Setting {
+	 OFF("---"), LOW("--+"), MEDIUM("-++"), HIGH("+++");
+	
+	private String value;
+	
+	private Setting(String value) {
+		this.value = value;
+	}
+	
+	public String toString() {
+		return value;
+	}
 
+	
 }
